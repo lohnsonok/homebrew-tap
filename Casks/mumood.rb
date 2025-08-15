253ea2e333c8e5a391210da932e4825373efef64
Lionel Okpeicha <lohnsonok@gmail.com>
2025-08-15 21:44:30 +0200

    docs: add translated version of report in docx format

--- Casks/mumood.rb
+++ Casks/mumood.rb
@@ -5,14 +5,14 @@ cask "mumood" do
   sha256 arm:   "cb8b7d9e1e78fc42875849336159aeedb2efa8b079f195efe93c56ccab770bf9",
          intel: "45e4a88638a3f4b4edcd04f68b24fde6a991eeaad19787fb3a305777524fb0d7"
 
-  url "https://mumood.app/api/download?arch=#{arch}&tag=v#{version}",
-      verified: "mumood.app"
+  url "https://mumood.com/api/download?arch=#{arch}&tag=v#{version}",
+      verified: "mumood.com"
   name "MuMood"
   desc "Wallpaper engine that adapts to your music (Apple Music/Spotify)"
-  homepage "https://mumood.app"
+  homepage "https://mumood.com"
 
   livecheck do
-    skip "Updates announced on https://mumood.app"
+    skip "Updates announced on https://mumood.com"
   end
 
   depends_on macos: ">= :ventura"
